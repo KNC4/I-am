cb9c3db518dc84a2435eda6ec2edef1c463f917e
Kendall <kendallnavarro14@gmail.com>
2023-01-31 18:22:49 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+  
+    var btnScrollDown = document.querySelector('#scroll_down');
+  
+    function scrollDown() {
+      var windowCoords = document.documentElement.clientHeight * .90 ;
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 7);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
+  
+    btnScrollDown.addEventListener('click', scrollDown);
+  })();
