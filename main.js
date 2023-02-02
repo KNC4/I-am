173a7301e964d7383ba8a77ecab45e1983ac1c6f
Kendall <kendallnavarro14@gmail.com>
2023-02-01 21:32:47 -0600

    typeit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
+
 (function() {
     'use strict';
-  
     var btnScrollDown = document.querySelector('#scroll_down');
   
     function scrollDown() {
@@ -18,46 +19,18 @@
   
     btnScrollDown.addEventListener('click', scrollDown);
   })();
-      function type(i, t, ie, oe) {
-        input = document.getElementById(ie).innerHTML;
-        document.getElementById(oe).innerHTML += input.charAt(i);
-        setTimeout(function(){
-            ((i < input.length - 1) ? type(i+1, t, ie, oe) : false);
-        }, t);
-    }
 
-  type(0, 100, "text", "screen");
-
-  async function typeSentence(sentence, eleRef, delay = 100) {
-    const letters = sentence.split("");
-    let i = 0;
-    while(i < letters.length) {
-      await waitForMs(delay);
-      $(eleRef).append(letters[i]);
-      i++
-    }
-    return;
-  }
-  
-  function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  typeSentence("Hey, over here!", "#sentence");
-
-  async function deleteSentence(eleRef) {
-    const sentence = $(eleRef).html();
-    const letters = sentence.split("");
-    let i = 0;
-    while(letters.length > 0) {
-      await waitForMs(100);
-      letters.pop();
-      $(eleRef).html(letters.join(""));
-    }
-  }
-
-  $( document ).ready(async function() {
-    await typeSentence("Mr. Stark, I don't feel so good..", "#sentence");
-    await waitForMs(2000);
-    deleteSentence("#sentence");
-  });
+  document.addEventListener("DOMContentLoaded", function () {
+    new TypeIt("#text", {
+  //strings: ["This is my string!"],
+})
+    .type("I was", { delay: 1000 })
+    .delete(3)
+    .type("am")
+    .pause(1000)
+    .delete(2)
+    .type("will be")
+    .pause(700)
+    .type(".")
+    .go();
+    });
