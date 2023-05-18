2b14701b142dee3cd6e1285f80844f6a29ea25f1
Kendall <kendallnavarro14@gmail.com>
2023-05-18 13:04:02 -0600

    cars?

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+var scene2 = document.getElementById('scene2');
+var parallaxInstance2 = new Parallax(scene2);
